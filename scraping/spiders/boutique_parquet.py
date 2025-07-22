10faf95581e1db879b573cd3a6438efac00ef8ff
Thomas Le Floch <t.lefloch53@gmail.com>
2025-07-22 14:41:02 +0200

    récupération du nom et de la page liste des catégories

--- scraping/spiders/boutique_parquet.py
+++ scraping/spiders/boutique_parquet.py
@@ -6,5 +6,27 @@ class BoutiqueParquetSpider(scrapy.Spider):
     allowed_domains = ["boutique-parquet.com"]
     start_urls = ["https://boutique-parquet.com"]
 
-    def parse(self, response):
-        pass
+    menu_id = "#header-menu-all-product"
+
+    def parse_sub_categories(self, response):
+        sub_categories = response.css(f'{self.menu_id} li.level1 > a')
+        for category in sub_categories:
+            yield {
+                'name': category.css('span::text').get(),
+                'page_list': category.attrib['href']
+            }
+    
+    def parse_upper_categories(self, response):
+        upper_categories = response.css(f'{self.menu_id} li.level0 > a')
+        for category in upper_categories:
+            yield {
+                'name': category.css('span::text').get()
+            }
+
+    def parse(self, response): 
+        yield from self.parse_upper_categories(response)
+        yield from self.parse_sub_categories(response)
+    
+
+
+        
