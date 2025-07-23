71ddf503ff1a9821dcf14fa44cbe66c7a579bfcd
thomas-lefloch <56230090+thomas-lefloch@users.noreply.github.com>
2025-07-23 11:13:50 +0200

    fix: typo

--- scraping/pipelines/pipeline_filter.py
+++ scraping/pipelines/pipeline_filter.py
@@ -5,7 +5,7 @@ class FilterCategoryPipeline:
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
         category_id = adapter.get("id", "")
-        # D'apres notre analyse sur le site, on a remarqué que les produits présent sur les marques apparaissent deja dans d'aures catégories
+        # D'apres notre analyse sur le site, on a remarqué que les produits présent sur les marques apparaissent deja dans d'autres catégories
         if category_id.startswith("/marque"):
             raise scrapy.exceptions.DropItem(f"Catégorie exclue : {category_id}")
         
