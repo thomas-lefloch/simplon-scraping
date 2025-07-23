899bed7163a9bfd9c296ac17188db44025b3e2bc
Sebastien Dewaelle <sdewaelle@gmail.com>
2025-07-23 16:45:04 +0200

    Ajout du champ TVA + champs obligatoires dans l'item_products

--- scraping/items/item_products.py
+++ scraping/items/item_products.py
@@ -3,11 +3,12 @@ from scrapy.item import Item, Field
 
 
 class ItemProducts(scrapy.Item):
-    id = Field()  # sku
-    name = Field()
+    id = Field(required=True)  # sku
+    name = Field(required=True)
     base_price = Field()  # Prix du produit hors promo (en euro)
-    price = Field()  # Prix actuel du produit (en euro)
-    unit = Field()  # Unite (m2 / kg / unité)
-    url = Field()
+    price = Field(required=True)  # Prix actuel du produit (en euro)
+    vat_included = Field(required=True)  # Booléen qui indique si la TVA est incluse
+    unit = Field()  # Unite (m2 / kg / unité / ml)
+    url = Field(required=True)
     category_id = Field()
     image = Field()
